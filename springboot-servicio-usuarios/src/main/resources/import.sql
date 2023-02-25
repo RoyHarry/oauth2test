76cd4152f142766d70e2bbc8cae4830888db36d4
Roy Colorado <rcolorado@wabiproject.com>
2023-02-24 20:43:27 -0500

    se corrige el nombre de los campos de roles

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,2 +1,9 @@
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES('roy', '1234', 1, 'Roy', 'Colorado', 'rcolorado01@gmail.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES('admin', '1234', 1, 'Roy', 'Colorado', 'rcolorado01@gmail.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES('admin', '1234', 1, 'Roy', 'Colorado', 'roy.colorado.asencio@gmail.com');
+
+INSERT INTO roles (nombre) values ('ROLE_USER');
+INSERT INTO roles (nombre) values ('ROLE_ADMIN');
+
+INSERT INTO usuarios_roles (usuario_id, roles_id) values (1, 1);
+INSERT INTO usuarios_roles (usuario_id, roles_id) values (2, 2);
+INSERT INTO usuarios_roles (usuario_id, roles_id) values (2, 1);
