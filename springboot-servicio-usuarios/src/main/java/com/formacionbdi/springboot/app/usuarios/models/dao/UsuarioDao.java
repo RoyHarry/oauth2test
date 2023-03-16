7fd0cb43c87dbf85fa34f72e909d7a4635108790
Roy Colorado <rcolorado@wabiproject.com>
2023-03-15 20:09:18 -0500

    commos-usuarios

--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path="usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
